23a055b961ac69d954668b22ee4d43eabb53fe1a
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-15 10:40:55 +0530

    error

--- src/components/Blog/Post.jsx
+++ src/components/Blog/Post.jsx
@@ -9,6 +9,7 @@ export default function Post(props) {
       <div className="flex bg-gray-100 shadow-sm  flex-col p-2 gap-3  dark:bg-black border border-gray-500 rounded-lg min-w-220 md:w-auto">
         <Image src={code} alt="Post Thumbnail" width={220} height={220} className='rounded-lg' />
         <div className="flex flex-col gap-2">
+          <p className='hidden'>{props.id}</p>
           <p className='text-right text-gray-400 text-sm'>18 feb 2024</p>
           <p className='text-lg font-bold'>{props.title}</p>
           <p className='text-gray-400 text-sm'>{props.content}</p>
