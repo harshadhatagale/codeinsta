2fe2cdb21f8cb779323a2315811133e397efd8d3
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-15 09:07:08 +0530

    error resolve 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import { Black_And_White_Picture, Inter } from "next/font/google";
-import { Main } from "next/document";
+
 import LatestPostListView from "@/pages/LatestPostListView";
 import Header from "@/components/header1";
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <main >
