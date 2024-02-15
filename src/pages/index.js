e2909c17ec2c3a3773e7845290a74f0dfbca7016
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-15 09:10:13 +0530

    error resolove

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import LatestPostListView from "@/pages/LatestPostListView";
 import Header from "@/components/header1";
 export default function Home() {
   return (
-    <main >
+     <>
       <Header content="Latest Posts"/>
       <LatestPostListView />
-    </main>
+     </>
   );
 }
