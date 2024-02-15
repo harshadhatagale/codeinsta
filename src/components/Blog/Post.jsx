776d4e1a3ba927de28413b9d8ea417edeb273f9a
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-15 10:34:23 +0530

    Popular Posts

--- src/components/Blog/Post.jsx
+++ src/components/Blog/Post.jsx
@@ -3,16 +3,15 @@ import blog_thumb from "../../../public/blog_thumb.jpg"
 import Image from 'next/image'
 import Link from 'next/link'
 import code from "../../../public/code.jpg"
-export default function Post() {
+export default function Post(props) {
   return (
     <>
       <div className="flex bg-gray-100 shadow-sm  flex-col p-2 gap-3  dark:bg-black border border-gray-500 rounded-lg min-w-220 md:w-auto">
         <Image src={code} alt="Post Thumbnail" width={220} height={220} className='rounded-lg' />
         <div className="flex flex-col gap-2">
           <p className='text-right text-gray-400 text-sm'>18 feb 2024</p>
-          <p className='text-lg font-bold'>Post Title</p>
-          <p className='text-gray-400 text-sm'>Harshad</p>
-          <p className='text-gray-400 text-sm'>This is post 1...</p>
+          <p className='text-lg font-bold'>{props.title}</p>
+          <p className='text-gray-400 text-sm'>{props.content}</p>
           <div className="flex items-center justify-center p-x-5 p-y-10">
             <Link href="/" className='text-white rounded-md w-full bg-green-700 text-center'>Read This!</Link>
           </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,65 @@
 
-import LatestPostListView from "@/pages/LatestPostListView";
 import Header from "@/components/header1";
-export default function Home() {
+import LatestPost from "./latestPosts";
+import PopularPost from "@/pages/popularPosts";
+export async function getStaticProps(){
+  const latestPosts= [
+    {
+      id:1, 
+      title: "Post 1",
+      content:"This is Post 1",
+    },
+    {
+      id:2, 
+      title: "Post 2",
+      content:"This is Post 2",
+    },
+    {
+      id:3, 
+      title: "Post 3",
+      content:"This is Post 3",
+    },
+    {
+      id:4, 
+      title: "Post 4",
+      content:"This is Post 4",
+    }
+  ];
+  const popularPosts= [
+    {
+      id:1, 
+      title: "Post 1",
+      content:"This is Post 1",
+    },
+    {
+      id:2, 
+      title: "Post 2",
+      content:"This is Post 2",
+    },
+    {
+      id:3, 
+      title: "Post 3",
+      content:"This is Post 3",
+    },
+    {
+      id:4, 
+      title: "Post 4",
+      content:"This is Post 4",
+    }
+  ];
+  return{
+    props: {
+      latestPosts,popularPosts
+    },
+  }
+}
+export default function Home({latestPosts, popularPosts}) {
   return (
      <>
       <Header content="Latest Posts"/>
-      <LatestPostListView />
+      <LatestPost data={latestPosts} />
+      <Header content="Popular Posts"/>
+      <PopularPost data={popularPosts}/>
      </>
   );
 }
