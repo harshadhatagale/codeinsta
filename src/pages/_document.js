4082113caef620cf6093494b1144482a9204a430
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-16 10:35:33 +0530

    adsence

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <title>Code Insta</title>
+      <meta name="google-adsense-account" content="ca-pub-3944085814863048"></meta>
       <Head />
       <body  className="dark:bg-black white dark:text-white text-black">
         <Main />
