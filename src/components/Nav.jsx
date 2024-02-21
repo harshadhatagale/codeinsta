09cfd6fa6d55e93dea853ffd8b5f1414191be8b1
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-21 19:45:40 +0530

    post 3

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,7 +21,7 @@ export default function Nav() {
     <nav className={` fixed top-0 border-t bg-white dark:bg-black nav ${isNavOpen ? "active":""} border-b dark:border-gray-500 flex h-20  justify-around w-screen items-center px-3`}>
     <div className=" h-20 flex w-screen logo_ham sm:max-auto md:w-auto justify-around items-center">
     <div className="tracking-wider text-3xl font-extrabold">
-         <Link href="/" className={`${roboto.className} logo transition-all text-white serif`} onClick={CloseNav}>Code Insta</Link>
+         <Link href="/blog" className={`${roboto.className} logo transition-all text-white serif`} onClick={CloseNav}>Code Insta</Link>
      </div>
      <div className="flex items-center justify-center gap-x-2">
      <div className={`${isNavOpen ? "active":""} hamburger cursor-pointer md:hidden`} onClick={OpenNav}>
@@ -33,9 +33,7 @@ export default function Nav() {
      </div>
     <div className={` ${isNavOpen ? "active":""} links hidden md:flex`}>
      <ul className='flex items-center text-black font-bold justify-center gap-8 dark:text-white lg:text-xl'>
-       <li className='hover:text-cyan-400'><Link href="/" onClick={CloseNav}>Home</Link></li>
        <li className='hover:text-cyan-400'><Link href="/blog" onClick={CloseNav}>Blog</Link></li>
-       <li className='hover:text-cyan-400'><Link href="/learn" onClick={CloseNav}>Learn</Link></li>
      </ul>
     </div>
  </nav>
