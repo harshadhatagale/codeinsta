773f3551dd1b2d7a46d7bf39a106ceee8055cfe6
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-21 22:50:26 +0530

    Google Analytics2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <title>Code Insta</title>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-YF8ZZVMNVE"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-J8N20BZMEN"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
 
-        gtag('config', 'G-YF8ZZVMNVE');
+        gtag('config', 'G-J8N20BZMEN');
       </script>
       <Head />
       <body className="dark:bg-black white dark:text-white text-black">
