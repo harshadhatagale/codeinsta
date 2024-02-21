39a1ed338a554f3900ae25375824c7078bc8f8d3
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-21 23:03:42 +0530

    Google Analytics2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import ReactGa from "react-ga"
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 export default function App({ Component, pageProps }) {
+  const router= useRouter();
+  useEffect(()=>{
+    ReactGa.initialize("G-J8N20BZMEN");
+    ReactGa.pageview(window.location.pathname+window.location.search);
+    const handleRouteChange=(url)=>{
+      ReactGa.pageview(url)
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return()=>{
+      router.events.off("routeChangeComplete", handleRouteChange)
+    };
+  }, [])
   return (
     <Layout>
       <main>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript, } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <title>Code Insta</title>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-J8N20BZMEN"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-J8N20BZMEN');
-      </script>
       <Head />
       <body className="dark:bg-black white dark:text-white text-black">
         <Main />
