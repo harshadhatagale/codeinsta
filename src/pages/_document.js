76efc76ae510df224a1c52b7759d3bb4e6e627b7
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-22 19:45:59 +0530

    tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript, } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <title>Code Insta</title>
+      
       <Head />
       <body className="dark:bg-black white dark:text-white text-black">
         <Main />
