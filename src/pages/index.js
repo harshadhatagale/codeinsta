7728f7efe79aaa3a8ffc4ebda4df304c32d3e6ad
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-22 23:41:44 +0530

    tag manager

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,14 @@
 import Header from "@/components/header1";
 import LatestPost from "./latestPosts";
 import PopularPost from "@/pages/popularPosts";
+import { Head } from "next/document";
 export default function Home() {
   return (
-     <>
-      <Header content="Latest Posts"/>
-     </>
+    <>
+      <Head>
+        <meta name="description" content="Explore comprehensive coding tutorials on our blog, where we demystify programming concepts, provide hands-on examples, and guide you through real-world projects. From beginner-friendly introductions to advanced coding challenges, our tutorials cover a wide range of programming languages and technologies. Elevate your coding skills with our expert insights and practical guidance. Start your coding journey today!" />
+      </Head>
+      <Header content="Latest Posts" />
+    </>
   );
 }
