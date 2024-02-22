eb86b9714a560abc1d4483dedef383d76bd6a810
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-22 23:57:02 +0530

    descriptin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 
 import Header from "@/components/header1";
-import LatestPost from "./latestPosts";
-import PopularPost from "@/pages/popularPosts";
 import { Head } from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta name="description" content="Explore comprehensive coding tutorials on our blog, where we demystify programming concepts, provide hands-on examples, and guide you through real-world projects. From beginner-friendly introductions to advanced coding challenges, our tutorials cover a wide range of programming languages and technologies. Elevate your coding skills with our expert insights and practical guidance. Start your coding journey today!" />
-      </Head>
-      <Header content="Latest Posts" />
+    <meta name="description" content="Explore comprehensive coding tutorials on our blog, where we demystify programming concepts, provide hands-on examples, and guide you through real-world projects. From beginner-friendly introductions to advanced coding challenges, our tutorials cover a wide range of programming languages and technologies. Elevate your coding skills with our expert insights and practical guidance. Start your coding journey today!" />
+    <div>
+    <Header content="Latest Posts" />
+    </div>
     </>
   );
 }
