f5b3e25a26d11f0f96264796dacd51c336322bff
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-23 23:12:17 +0530

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import HeadTag from "@/components/HeadTag";
 export default function Home() {
   return (
     <>
-    <HeadTag title="Code Insta" description="Explore comprehensive coding tutorials on our blog, where we demystify programming concepts, provide hands-on examples, and guide you through real-world projects. From beginner-friendly introductions to advanced coding challenges, our tutorials cover a wide range of programming languages and technologies" canUrl="https://www.codeinsta.tech/blog"/>
+    <HeadTag title="Code Insta" description="Explore comprehensive coding tutorials on our blog, where we demystify programming concepts, provide hands-on examples, and guide you through real-world projects. From beginner-friendly introductions to advanced coding challenges, our tutorials cover a wide range of programming languages and technologies" canUrl="https://codeinsta.tech/blog"/>
     <div>
     <Header content="Latest Posts" />
     </div>
