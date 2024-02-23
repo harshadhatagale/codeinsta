dc538b600f0539a8ac1b6e7fd5ae86e357a371cd
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-23 12:28:28 +0530

    optimizing script tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,26 +6,26 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-D27GE94RQC"></Script>
-        <Script dangerouslySetInnerHTML={{
-          __html: `
+      </Head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-D27GE94RQC"></Script>
+      <Script dangerouslySetInnerHTML={{
+        __html: `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   
   gtag('config', 'G-D27GE94RQC');
   `
-        }} />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      }} />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-W97JL5XZ');`,
-          }}
-        />
-      </Head>
+        }}
+      />
       <Layout>
         <main>
           <Component {...pageProps} />
