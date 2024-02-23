e14e0f10936a6f35314801804df1f9f26ec34bf4
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-23 18:08:06 +0530

    url

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -9,20 +9,24 @@ import "highlight.js/styles/dracula.css"
 import styles from "../../styles/post_detail_view.module.css"
 import Head from "next/head"
 import HeadTag from '@/components/HeadTag';
+
+
 const merry= Merriweather({
   subsets:["latin"],
   weight:["300","400","700","900"]
 })
 export default function DetailPostView({ post }) {
   const router = useRouter();
+  const currentUrl= router.asPath;
   if (router.isFallback) {
 
     return
     <div>Loading</div>
   }
+
   return (
     <>
-     <HeadTag title={post.slug.replace(".md","")} description={post.content.slice(0,160)} canUrl={`https://www.codeinsta.tech/blog/${post.slug}`}/>
+     <HeadTag title={post.slug.replace(".md","")} description={post.content.slice(0,160)} canUrl={currentUrl}/>
     <div className=" dark:text-white lg:text-lg leading-loose lg:px-20 md:px-18 px-5">
     <div>
        <Header2 content= {post.slug.replace(".md","")} thumbnail={post.thumbnail} date={post.date} />
