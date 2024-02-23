523ea872c868dc45746320af008519f3ba7dbfc0
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-23 14:21:21 +0530

    google tag manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,25 +5,15 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-D27GE94RQC"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  
-  gtag('config', 'G-D27GE94RQC');
-  `
-        }} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<script dangerouslySetInnerHTML={{
+  __html:`
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W97JL5XZ');`,
-          }}
-        />
+})(window,document,'script','dataLayer','GTM-PP9M53RB');
+  `
+}}/>
       </Head>
       <Layout>
         <main>
