76a67303eb4f5d9f7d99206c5bdc008d20b28fbf
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-23 16:48:34 +0530

    canonical

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,16 +5,17 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-      <meta name="google-site-verification" content="cC14gQ_L4efyo09WY__vfk659sty_bFqlShlO3Z1OyY" />
-<script dangerouslySetInnerHTML={{
-  __html:`
+        <meta name="google-site-verification" content="cC14gQ_L4efyo09WY__vfk659sty_bFqlShlO3Z1OyY" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PP9M53RB');
   `
-}}/>
+        }} />
+        <link rel="canonical" href="https://www.codeinsta.tech/"  />
       </Head>
       <Layout>
         <main>
